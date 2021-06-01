faf6f0d3da4ae620f636dbe39c2aebbf660cefde
Salem <48455909+sbinmakhashen@users.noreply.github.com>
2021-06-01 14:29:01 -0400

    click event listener

--- Day-3-Counter/App.js
+++ Day-3-Counter/App.js
@@ -1,6 +1,7 @@
 const counter = document.querySelector('.count');
 const plusBtn = document.getElementById('plus');
 const minusBtn = document.getElementById('minus');
+const resetBtn = document.querySelector('.reset-btn');
 let count = 0;
 // click listener for plusBtn
 plusBtn.addEventListener('click', () => {
@@ -11,5 +12,11 @@ plusBtn.addEventListener('click', () => {
 // click listener for minusBtn
 minusBtn.addEventListener('click', () => {
   count -= 5;
+  counter.innerHTML = count;
+});
+
+// Reset button
+resetBtn.addEventListener('click', () => {
+  count = 0;
   counter.innerHTML = count;
 });
