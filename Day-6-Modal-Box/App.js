e2d6ac3994735c4bb70d0e2dbd103b1c98c8f472
Salem <48455909+sbinmakhashen@users.noreply.github.com>
2021-06-03 00:42:29 -0400

    Update App.js

--- Day-6-Modal-Box/App.js
+++ Day-6-Modal-Box/App.js
@@ -1,11 +1,13 @@
 // vars
 const btn = document.getElementById('btn')
 const modalSec = document.getElementById('modal-sec')
+const closeBtn = document.getElementById('close')
 const recipeTitle = document.querySelector('.recipe-title')
 const recipeImg = document.querySelector('.img')
 const recipeDes = document.querySelector('.desc')
 
 btn.addEventListener('click', () => {
+    modalSec.style.display = 'block'
   const r = fetch('https://www.themealdb.com/api/json/v1/1/random.php').then(res => res.json())
     .then(data => {
         // title of the recipe
@@ -15,3 +17,5 @@ btn.addEventListener('click', () => {
         console.log(data.meals[0])
     })
 });
+
+closeBtn.addEventListener('click', () => modalSec.style.display = 'none')
