e01a5e212c9575c74b612deb6ae07f50e40e671a
Salem <48455909+sbinmakhashen@users.noreply.github.com>
2021-06-02 20:05:52 -0400

    Add files via upload

---
+++ Day-6-Modal-Box/App.js
@@ -0,0 +1,17 @@
+// vars
+const btn = document.getElementById('btn')
+const modalSec = document.getElementById('modal-sec')
+const recipeTitle = document.querySelector('.recipe-title')
+const recipeImg = document.querySelector('.img')
+const recipeDes = document.querySelector('.desc')
+
+btn.addEventListener('click', () => {
+  const r = fetch('https://www.themealdb.com/api/json/v1/1/random.php').then(res => res.json())
+    .then(data => {
+        // title of the recipe
+        recipeTitle.innerHTML = data.meals[0].strMeal
+        recipeImg.src = data.meals[0].strMealThumb
+        recipeDes.innerHTML = data.meals[0].strInstructions
+        console.log(data.meals[0])
+    })
+});
