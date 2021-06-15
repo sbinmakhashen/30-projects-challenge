be8df21447b3492260cd89bb335d993f557afdb5
Salem <48455909+sbinmakhashen@users.noreply.github.com>
2021-06-15 19:57:57 -0400

    generating a new chapter on click

--- Day-13-Quran-Chapter/App.js
+++ Day-13-Quran-Chapter/App.js
@@ -2,17 +2,38 @@
 const chapter = document.getElementById('chapter');
 const btn = document.getElementById('btn');
 // getting random number from 0 to 114 becuase there are 114 chapters in the holy book of Quran
-const randomNum = Math.floor(Math.random() * 114);
+
 // chapter url from quran api
-const url = `https://api.quran.com/api/v4/quran/verses/imlaei?chapter_number=${randomNum}`;
-// console.log(url);
-btn.addEventListener('click', () => {});
-randomChapter();
 // Async/await function
-async function randomChapter() {
-  const res = await fetch(url);
+// const cha = async () => {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   };
+
+//   const res = await fetch(url, config);
+//   const data = await res.json();
+//   return (chapter.innerHTML = data.verses.map(
+//     (v) => ` ${v.verse_key}  ${v.text_imlaei}`
+//   ));
+// };
+// cha();
+// console.log(cha());
+
+btn.addEventListener('click', async () => {
+  const randomNum = Math.floor(Math.random() * 114) + 1;
+  const url = `https://api.quran.com/api/v4/quran/verses/imlaei?chapter_number=${randomNum}`;
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+  const res = await fetch(url, config);
   const data = await res.json();
-  data.verses.forEach((el) => {
-    chapter.innerHTML = el.text_imlaei;
-  });
-}
+  // data.verses.map((v) => console.log(url));
+  return (chapter.innerHTML = data.verses.map(
+    (v) => ` ${v.text_imlaei}
+    <span class='span'>${v.verse_key}</span>`
+  ));
+});
