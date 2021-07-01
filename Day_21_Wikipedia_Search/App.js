ff5b324e594b0b0c7bcfce89b00e6be9d05a0e7e
Salem <48455909+sbinmakhashen@users.noreply.github.com>
2021-07-01 19:03:16 -0400

    Update App.js

--- Day_21_Wikipedia_Search/App.js
+++ Day_21_Wikipedia_Search/App.js
@@ -1,33 +1,64 @@
-// vars
+// var
 const form = document.getElementById('form');
 const input = document.getElementById('text');
 const btn = document.querySelector('.btn');
+const results = document.getElementById('results');
 const url =
   'https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=';
+
+// form event listener
 form.addEventListener('submit', (event) => {
+  // prevent defualt from happening
   event.preventDefault();
-  // validate
-  if (input.value === '') {
-    console.log('enter something');
+
+  if (input.value === '' || input.value === ' ') {
+    return errorMsg('The search is blank!, search for something please!');
   } else {
-    dataSearch(input.value);
+    return wikiData(input.value);
   }
 });
 
-async function dataSearch(val) {
+input.focus();
+
+// this async/await function will contain the fetch method and search queries
+async function wikiData(val) {
   const res = await fetch(url + val);
   const data = await res.json();
-  // validation
+  console.log(data);
+  // validate
   if (data.query.search.length === 0) {
-    return console.log('enter something');
+    return notFound(`Sorry!! we didnt find any data for '${input.value}'`);
   } else {
-    resultDisplay(data);
+    // run this function that will display my data
+    return displayData(data);
   }
 }
 
-// displaying the results
-function resultDisplay(data) {
-  data.query.search.map((result) => {
-    console.log(result);
+// display my data
+function displayData(data) {
+  let newHtml = ``;
+  // looping through the data
+  data.query.search.map((prop) => {
+    let link = `https://en.wikipedia.org/?curid=${prop.pageid}`;
+    newHtml += `
+    <div class='res-container'>
+    <a href="${link}" target='_blank'>
+    <h2 class='title'>${prop.title} <small>(Click me to learn me)</small></h2>
+    </a>
+    <p>Description: ${prop.snippet}....</p>
+    <p class='time-stamp'><span>Last updated </span>: ${prop.timestamp}</p>
+    </div>`;
+    console.log(prop);
+    results.innerHTML = newHtml;
   });
+}
+
+// error message function for empty string
+function errorMsg(msg) {
+  results.innerHTML = `<h3 class = 'danger'>${msg}</h3>`;
+}
+
+// show messgae when the search was not found
+function notFound(msg) {
+  results.innerHTML = `<h3 class = 'danger'>${msg}</h3>`;
 }
