d9b5a3058fba344cd233d383bd44e55179711307
Salem <48455909+sbinmakhashen@users.noreply.github.com>
2021-07-09 18:07:01 -0400

    Add files via upload

---
+++ Day_24_Password_generator/App.js
@@ -0,0 +1,11 @@
+// getting DOM elemnets
+const input = document.getElementById('pass');
+const btn = document.querySelector('.btn');
+
+// password characters
+const PasswordChars =
+  'ABCDEFGHIKLMNOPQRSTVXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
+// event listener for a click
+btn.addEventListener('click', getPassword);
+
+function getPassword() {}
