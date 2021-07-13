d88e8060df34b234ead53559d2cc27b8b8c6a7cf
Salem <48455909+sbinmakhashen@users.noreply.github.com>
2021-07-12 23:47:18 -0400

    Delete Day-4-Search-Widget directory

--- Day-4-Search-Widget/App.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector('.input');
-const btn = document.querySelector('.btn');
-const searchDiv = document.querySelector('.search');
-
-btn.addEventListener('click', () => {
-  searchDiv.classList.toggle('active');
-  input.focus();
-});
