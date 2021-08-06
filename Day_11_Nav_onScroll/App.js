b43c80318e880f82fffd6402fb458667b8b7508a
Salem <48455909+sbinmakhashen@users.noreply.github.com>
2021-08-06 00:15:53 -0400

    Delete Day_11_Nav_onScroll directory

--- Day_11_Nav_onScroll/App.js
+++
@@ -1,7 +0,0 @@
-// listen for a scroll event
-window.addEventListener('scroll', () => {
-  const header = document.querySelector('.header');
-  console.log(header);
-  header.classList.toggle('sticky', scrollY > 50);
-  console.log(scrollY);
-});
