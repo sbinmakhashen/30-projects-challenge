25f3cd8b5f341b7cd3fd4fe00edc4058d2ed7236
Salem <48455909+sbinmakhashen@users.noreply.github.com>
2021-08-05 23:43:08 -0400

    Delete 5-generate-random-hexColor directory

--- 5-generate-random-hexColor/App.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById('btn');
-const text = document.getElementById('text');
-
-btn.addEventListener('click', () => {
-  const randomHexCo = Math.random().toString(16).substring(2, 8);
-  const body = document.body;
-  body.style.backgroundColor = `#${randomHexCo}`;
-  text.innerHTML = `#${randomHexCo}`;
-});
